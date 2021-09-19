8fca856a3c3f9a1778c5e8d7625cfb48e3715f55
MoSaber <msaberms72016@gmail.com>
2021-09-19 15:29:37 +0200

    first commit

---
+++ MoviesAppTask_GizaSys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesAppTask_GizaSys
+//
+//  Created by Saber on 19/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
